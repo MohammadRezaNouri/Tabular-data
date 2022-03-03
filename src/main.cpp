54c6fdd9459e85267c9d4945922e0da747c82d1f
MohammadRezaNouri <MohammadRezaNouri01@Gmail.Com>
2022-03-03 23:53:59 +0330

    Add library and header and main function in main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <thread>
+#include <chrono>
+#include <sstream>
+#include <algorithm>
+
+#include "table.h"
+#include "invalid_col.h"
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
