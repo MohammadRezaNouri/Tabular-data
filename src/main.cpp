5e40c73a881b3bd82806166258422b02545e5439
MohammadRezaNouri <MohammadRezaNouri01@Gmail.Com>
2022-03-04 00:05:53 +0330

    export command in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -209,5 +209,29 @@ menu:
             goto menu;
         }
     }
+    else if (tokens[0] == "export")
+    {
+        if (tokens.size() == 2)
+        {
+            cerr << "No name is saved for file storage." << endl;
+            tokens.erase(tokens.begin(), tokens.end());
+            goto menu;
+        }
+        Bool = 0;
+        for (int i = 0; i < Table.size(); i++)
+            if (tokens[1] == Table[i]->getName())
+            {
+                Bool = 1;
+                Table[i]->Export(tokens[2]);
+                tokens.erase(tokens.begin(), tokens.end());
+                goto menu;
+            }
+        if (Bool == 0)
+        {
+            cerr << "There is no table with this name." << endl;
+            tokens.erase(tokens.begin(), tokens.end());
+            goto menu;
+        }
+    }
     return 0;
 }
