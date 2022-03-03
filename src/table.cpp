82e46a145ab6211d719c11c4d3efadac5de2bbff
MohammadRezaNouri <MohammadRezaNouri01@Gmail.Com>
2022-03-03 22:53:00 +0330

    Implementing the setCol in table.cpp

--- src/table.cpp
+++ src/table.cpp
@@ -9,3 +9,16 @@ void table::setName(string Name)
 {
     name = Name;
 }
+
+void table::setCol(string Col)
+{
+    for (int i = 0; i < Col.size(); i++)
+        if (Col[i] < '0' || Col[i] > '9')
+            throw invalid_col(0);
+    if (stoi(Col) < 0)
+        throw invalid_col();
+    col = stoi(Col);
+    _table = new float[row];
+    for (int i = 1; i <= row; i++)
+        _table[i - 1] = 0;
+}
