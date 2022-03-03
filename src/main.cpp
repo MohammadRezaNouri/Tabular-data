179b248465271bfa7478ec0c3df8b7d5de0dd296
MohammadRezaNouri <MohammadRezaNouri01@Gmail.Com>
2022-03-03 23:59:19 +0330

    Basic work in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,14 @@ using namespace std;
 
 int main()
 {
-
+    system("clear || cls");
+    cout << "Hi, welcome to my program." << endl;
+    string t1;
+    vector<string> tokens;
+    vector<table *> Table;
+    int Bool;
+    chrono::seconds time(2);
+    this_thread::sleep_for(time);
+    cout << "Page cleaning = clear" << endl;
     return 0;
 }
